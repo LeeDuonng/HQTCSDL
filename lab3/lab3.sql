cfb14f0415ba3e7ba22e9d251b66611acbc1f61f
DuongLee <89838052+LeeDuonng@users.noreply.github.com>
2023-03-24 17:01:35 +0700

    update

--- lab3/lab3.sql
+++ lab3/lab3.sql
@@ -1,9 +1,9 @@
 ﻿--1. Hãy thống kê xem mỗi hãng sản xuất có bao nhiêu loại sản phẩm
-SELECT Hangsx.tenhang, COUNT(Sanpham.masp) AS so_luong_sp
+SELECT Hangsx.tenhang, COUNT(Sanpham.masp) AS SoLuongSp
 FROM Hangsx
 JOIN Sanpham ON Hangsx.mahangsx = Sanpham.mahangsx
 GROUP BY Hangsx.tenhang
-ORDER BY so_luong_sp DESC;
+ORDER BY SoLuongSp DESC;
 
 --2. Hãy thống kê xem tổng tiền nhập của mỗi sản phẩm trong năm 2018
 SELECT masp, SUM(soluongN * dongiaN) AS TongTienNhap
@@ -12,20 +12,20 @@ WHERE YEAR(ngaynhap) = 2020
 GROUP BY masp;
 
 --3. Hãy thống kê các sản phẩm có tổng số lượng xuất năm 2018 là lớn hơn 10.000 sản phẩm của hãng samsung.
-SELECT Sanpham.masp, Sanpham.tensp, SUM(Xuat.soluongX) AS tong_so_luong_xuat
+SELECT Sanpham.masp, Sanpham.tensp, SUM(Xuat.soluongX) AS TongSoLuongXuat
 FROM Sanpham JOIN Xuat ON Sanpham.masp = Xuat.masp
 WHERE Sanpham.mahangsx = 'H01'
 GROUP BY Sanpham.masp, Sanpham.tensp
 HAVING SUM(Xuat.soluongX) > 10000;
 
 --4. Thống kê số lượng nhân viên Nam của mỗi phòng ban.
-SELECT phong, COUNT(*) as 'So_luong_nhan_vien_nam'
+SELECT phong, COUNT(*) as SoLuongNhanVienNam
 FROM Nhanvien
 WHERE gioitinh = N'Nam'
 GROUP BY phong;
 
 --5. Thống kê tổng số lượng nhập của mỗi hãng sản xuất trong năm 2018.
-SELECT Hangsx.tenhang, SUM(Nhap.soluongN) AS tongnhap
+SELECT Hangsx.tenhang, SUM(Nhap.soluongN) AS TongNhap
 FROM Hangsx
 JOIN Sanpham ON Hangsx.mahangsx = Sanpham.mahangsx
 JOIN Nhap ON Sanpham.masp = Nhap.masp
@@ -33,7 +33,7 @@ WHERE YEAR(Nhap.ngaynhap) = 2020
 GROUP BY Hangsx.tenhang
 
 --6. Hãy thống kê xem tổng lượng tiền xuất của mỗi nhân viên trong năm 2018 là bao nhiêu.
-SELECT Nhanvien.manv, Nhanvien.tennv, SUM(Xuat.soluongX * Sanpham.giaban) AS tongtienxuat
+SELECT Nhanvien.manv, Nhanvien.tennv, SUM(Xuat.soluongX * Sanpham.giaban) AS TongTienXuat
 FROM Xuat
 INNER JOIN Sanpham ON Xuat.masp = Sanpham.masp
 INNER JOIN Nhanvien ON Xuat.manv = Nhanvien.manv
@@ -41,7 +41,7 @@ WHERE YEAR(Xuat.ngayxuat) = 2018
 GROUP BY Nhanvien.manv, Nhanvien.tennv
 
 --7. Hãy đưa ra tổng tiền nhập của mỗi nhân viên trong tháng 8 – năm 2018 có tổng giá trị lớn hơn 100.000
-SELECT manv, SUM(soluongN * dongiaN) AS tong_tien_nhap
+SELECT manv, SUM(soluongN * dongiaN) AS TongTienNhap
 FROM Nhap
 WHERE MONTH(ngaynhap) = 8 AND YEAR(ngaynhap) = 2018
 GROUP BY manv
